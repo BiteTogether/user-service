a32c6587b0acc459734c49bca145cfb14f1ae492
thanhnguyen2406 <nguyencongthanh2406@gmail.com>
2025-09-08 01:57:23 +0700

    main Set up repository

---
+++ src/test/java/com/bitetogether/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitetogether.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
